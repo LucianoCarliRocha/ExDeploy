5c8d3b2aa2f2021db99c5f3a443f06da8fbf0812
LucianoCarliRocha <sllucianodecarli@gmail.com>
2024-10-31 21:18:37 -0300

    segundo commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('items/', get_items, name='get_items'),
     path('items/create/', create_item, name='create_item'),
 ]
+
+from django.conf import settings 
+from django.conf.urls.static import static
+if settings.DEBUG:
+   urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
